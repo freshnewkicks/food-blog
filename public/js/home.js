8ad6d32bb3fd1ccd58172090d394e3bf96d3db1d
freshnewkicks <tylerdukedev@gmail.com>
2022-01-18 16:30:31 -0500

    previous/next blog post buttons work correctly

--- public/js/home.js
+++ public/js/home.js
@@ -11,6 +11,8 @@ let prevTitles = [];
 let prevBtn = document.querySelector('#prev-slider-btn');
 let nextBtn = document.querySelector('#next-slider-btn');
 prevBtn.style.opacity = '45%';
+prevBtn.classList.add('disabled');
+
 let cardTitles = document.querySelectorAll('.card-title');
 function toggleClass(x) {
     for (let i = 0; i < cardTitles.length; i++){
@@ -22,100 +24,74 @@ function toggleClass(x) {
     }
 }
 
-
-
 nextBtn.addEventListener('click', () => {
-    prevBtn.setAttribute('disabled', 'disabled');
-    setTimeout(() => {
-        prevBtn.removeAttribute( 'disabled');
-    }, 3000);
+        toggleClass('animate__slideInRight');
+        nextBtn.classList.add('disabled');
+        setTimeout(() => {prevBtn.classList.remove('disabled')}, 2000);
+        prevBtn.style.opacity = '100%';
+        nextBtn.style.opacity = '45%';
 
-    toggleClass('animate__slideInRight');
-    prevBtn.style.opacity = '100%';
-    nextBtn.style.opacity = '45%';
-
-    getAPI()
-        .then(data => {
-            // grab and store title elements
-            let titlesHtml = [
-                document.querySelector('#title-1'),
-            document.querySelector('#title-2'),
-            document.querySelector('#title-3')
-        ]
-            // grab text content of title elements and push to array
-            prevTitles.push(
-                titlesHtml[0].textContent,
-                titlesHtml[1].textContent,
-                titlesHtml[2].textContent
-            );
-            // grab data from fetch request and push to array
-            nextTitles.push(
-               data.at(-4).title,
-                data.at(-5).title,
-                data.at(-6).title
-            );
-            // iterate over each element and change to fetched data's
-            // next title
-            for (let i = 0; i < titlesHtml.length; i++){
-                titlesHtml[i].innerHTML = nextTitles[i];
-            }
-        })
-});
-
+        getAPI()
+            .then(data => {
+                // grab and store title elements
+                let titlesHtml = [
+                    document.querySelector('#title-1'),
+                    document.querySelector('#title-2'),
+                    document.querySelector('#title-3')
+                ]
+                // grab text content of title elements and push to array
+                prevTitles.push(
+                    titlesHtml[0].textContent,
+                    titlesHtml[1].textContent,
+                    titlesHtml[2].textContent
+                );
+                // grab data from fetch request and push to array
+                nextTitles.push(
+                    data.at(-4).title,
+                    data.at(-5).title,
+                    data.at(-6).title
+                );
+                // iterate over each element and change to fetched data's
+                // next title
+                for (let i = 0; i < titlesHtml.length; i++){
+                    titlesHtml[i].innerHTML = nextTitles[i];
+                }
+            })
+    });
 prevBtn.addEventListener('click', () => {
-    nextBtn.setAttribute('disabled', 'disabled');
-    setTimeout(() => {
-        nextBtn.removeAttribute( 'disabled');
-    }, 3000);
     toggleClass('animate__slideInLeft');
-    prevBtn.style.opacity = '45%';
-    nextBtn.style.opacity = '100%';
-    cardTitles.forEach(x => x.classList.toggle( 'animate__slideInRight'));
-    getAPI()
-        .then(data => {
-            // grab and store title elements
-            let titlesHtml = [
-                document.querySelector('#title-1'),
-                document.querySelector('#title-2'),
-                document.querySelector('#title-3')
-            ]
-            // grab text content of title elements and push to array
-            prevTitles.push(
-                titlesHtml[0].textContent,
-                titlesHtml[1].textContent,
-                titlesHtml[2].textContent
-            );
-            // grab data from fetch request and push to array
-            nextTitles.push(
-                data.at(-4).title,
-                data.at(-5).title,
-                data.at(-6).title
-            );
-            // iterate over each element and change to fetched data's
-            // next title
-            for (let i = 0; i < titlesHtml.length; i++){
-                titlesHtml[i].innerHTML = prevTitles[i];
-            }
-        })
-});
+    prevBtn.classList.add('disabled');
+    setTimeout(() => {nextBtn.classList.remove('disabled')}, 2000);
 
-    // badge.innerHTML = content.at(-4).category;
-    // cardTitle.innerHTML = content.at(-4).title;
-    // cardDesc.innerHTML = content.at(-4).desc;
-    // authors.innerHTML = content.at(-4).author;
-    // dateTime.innerHTML = content.at(-4).dateCreated;
-    // readTime.innerHTML = content.at(-4).readTime;
-    //
-    // badge.innerHTML = content.at(-5).category;
-    // cardTitle.innerHTML = content.at(-5).title;
-    // cardDesc.innerHTML = content.at(-5).desc;
-    // authors.innerHTML = content.at(-5).author;
-    // dateTime.innerHTML = content.at(-5).dateCreated;
-    // readTime.innerHTML = content.at(-5).readTime;
-    //
-    // badge.innerHTML = content.at(-6).category;
-    // cardTitle.innerHTML = content.at(-6).title;
-    // cardDesc.innerHTML = content.at(-6).desc;
-    // authors.innerHTML = content.at(-6).author;
-    // dateTime.innerHTML = content.at(-6).dateCreated;
-    // readTime.innerHTML = content.at(-6).readTime;
+    prevBtn.style.opacity = '45%';
+        nextBtn.style.opacity = '100%';
+        cardTitles.forEach(x => x.classList.toggle('animate__slideInRight'));
+
+        getAPI()
+            .then(data => {
+                // grab and store title elements
+                let titlesHtml = [
+                    document.querySelector('#title-1'),
+                    document.querySelector('#title-2'),
+                    document.querySelector('#title-3')
+                ]
+                // grab text content of title elements and push to array
+                prevTitles.push(
+                    titlesHtml[0].textContent,
+                    titlesHtml[1].textContent,
+                    titlesHtml[2].textContent
+                );
+                // grab data from fetch request and push to array
+                nextTitles.push(
+                    data.at(-4).title,
+                    data.at(-5).title,
+                    data.at(-6).title
+                );
+                // iterate over each element and change to fetched data's
+                // next title
+                for (let i = 0; i < titlesHtml.length; i++) {
+                    titlesHtml[i].innerHTML = prevTitles[i];
+                }
+            })
+    });
+
